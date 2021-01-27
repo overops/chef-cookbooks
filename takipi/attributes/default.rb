d7270afc006c8d1e0f9982eca0310b84f70838fc
Corey Severino <corey.severino@overops.com>
2021-01-27 15:33:50 -0500

    [OOE-289] Added Ubuntu and default use_fpm to true in the attribute.

--- takipi/attributes/default.rb
+++ takipi/attributes/default.rb
@@ -1,3 +1,4 @@
 default["takipi"]["secret_key"] = "YOUR SECRET KEY HERE"
+default["takipi"]["use_fpm"] = "true"
 default["takipi"]["machine_name"] = ""
 default["takipi"]["package_action"] = "install"
